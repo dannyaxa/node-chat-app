8ba6b57976dbcd4f4b39eb8b71b7980ab3638dfa
dan.tse <dan.tse@axa-groupsolutions.com>
2017-12-18 11:53:36 +0800

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
 const path = require('path');
+const express = require("express");
+const http = require('http');
+const socketIO = require('socket.io');
+const port = process.env.PORT || 3000;
 
 var publicPath = path.join(__dirname, "../public");
-
-const express = require("express");
-const port = process.env.PORT || 3000;
 var app = express();
-
 app.use(express.static(publicPath));
 
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New User Connected');
+});
+
+
+server.listen(port, () => {
   console.log(`server is up on ${port}`);
 });
 module.exports.app = app;
